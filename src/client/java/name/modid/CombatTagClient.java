88d7bbc35252246c26739f0317817e879265ed1b
Sean <contact@seanyang.me>
2025-03-09 13:38:45 -0400

    Make tabs spaces

--- src/client/java/name/modid/CombatTagClient.java
+++ src/client/java/name/modid/CombatTagClient.java
@@ -3,8 +3,8 @@ package name.modid;
 import net.fabricmc.api.ClientModInitializer;
 
 public class CombatTagClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
+    @Override
+    public void onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+    }
 }
