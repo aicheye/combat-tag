e49546d258384470fb829c17e263f0dd5c76a828
Sean Yang <contact@seanyang.me>
2025-07-09 01:27:29 -0400

    Refactor imports in ServerPlayerEntityMixin.java for improved readability

--- src/main/java/name/modid/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/name/modid/mixin/ServerPlayerEntityMixin.java
@@ -13,9 +13,9 @@ import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
-
 import net.minecraft.storage.ReadView;
 import net.minecraft.storage.WriteView;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
